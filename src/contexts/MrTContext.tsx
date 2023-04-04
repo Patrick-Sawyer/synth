0547e4c4a8637988da64297cd2220489fe51c9b2
Patrick Sawyer <patrick.sawyer@imgarena.com>
2023-04-04 11:51:31 +0100

    added mr t

--- src/contexts/MrTContext.tsx
+++ src/contexts/MrTContext.tsx
@@ -77,7 +77,7 @@ const Overlay = styled.div`
   left: 0;
   bottom: 0;
   right: 0;
-  background: rgba(0, 0, 0, 0.4);
+  background: rgba(0, 0, 0, 0.7);
   z-index: 1000000;
   display: flex;
   justify-content: center;
