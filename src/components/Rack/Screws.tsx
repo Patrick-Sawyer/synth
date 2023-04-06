f92f782ee5ba06cca2546733a9a7fabfacd598a5
Patrick Sawyer <patrick.sawyer@imgarena.com>
2023-04-06 08:42:46 +0100

    refactored screws

--- src/components/Rack/Screws.tsx
+++ src/components/Rack/Screws.tsx
@@ -1,38 +1,17 @@
-import { useEffect, useState } from "react";
+import { memo } from "react";
 import styled from "styled-components";
 import { Colors } from "../../utils/theme";
 import { Screw } from "./Screw";
 
-// TODO = CHANGE THIS TO STOP DOING ANYTHING ON RESIZE
+const screws = new Array(200).fill(null).map(() => <Screw />);
 
-export function Screws() {
-  const [screws, setScrews] = useState<Array<React.ReactElement>>([]);
-
-  useEffect(() => {
-    const resize = () => {
-      const width = Math.max(window.innerWidth, 500);
-      const numberOfScrews = Math.ceil(width / 13);
-      if (screws.length !== numberOfScrews) {
-        const newScrews = new Array(numberOfScrews)
-          .fill(null)
-          .map((_, index) => <Screw key={index} />);
-        setScrews(newScrews);
-      }
-    };
-    window.addEventListener("resize", resize);
-    resize();
-
-    return () => {
-      window.removeEventListener("resize", resize);
-    };
-  }, [screws.length]);
-
-  return <Wrapper>{[...screws]}</Wrapper>;
+function ScrewsComponent() {
+  return <Wrapper>{screws}</Wrapper>;
 }
 
 const Wrapper = styled.div`
   height: 15px;
-  width: 100%;
+  width: fit-content;
   background-color: ${Colors.screwBackground};
   display: flex;
   align-items: center;
@@ -40,4 +19,7 @@ const Wrapper = styled.div`
   padding: 0 4px;
   overflow: hidden;
   border-bottom: 1px solid ${Colors.darkBorder};
+  gap: 5px;
 `;
+
+export const Screws = memo(ScrewsComponent);
